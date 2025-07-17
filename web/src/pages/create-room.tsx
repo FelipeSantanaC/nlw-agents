34d2ae0bd8e576e73bf89b778374e41a2f2e2f41
FelipeSantanaC <felipecabocloo@gmail.com>
2025-07-17 17:57:47 -0300

    feat: implementando tela de criação e listagem de salas

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,13 +1,19 @@
 import { useQuery } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
 
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowRight } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
 type GetRoomsAPIResponse = Array<{
     id: string
     name: string
+    questionsCount: number
+    createdAt: string
 }>
 
 export function CreateRoom() {
-    const { data, isLoading } = useQuery({
+    const { data } = useQuery({
         queryKey: ['get-rooms'],
         queryFn: async () => {
             const response = await fetch('http://localhost:3333/rooms')
@@ -18,18 +24,40 @@ export function CreateRoom() {
     })
 
     return (
-        <div>
-            <div>Create Room</div>
+        <div className="min-h-screen py-8 px-4">
+            <div className="mx-auto max-w-4xl">
+                <div className="grid gap-8 grid-cols-2 items-start">
+                    <div/>
 
-            {isLoading && <p>Carregando...</p>}
-            <div className="flex flex-col gap-1">
-            {data?.map(room => {
-                return <Link key={room.id} to={`/room/${room.id}`}>{room.name}</Link>
-            })}
-            </div>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>
+                            Salas recentes
+                            </CardTitle>
+                            <CardDescription>
+                                Acesso rápido para as salas criadas recentemente
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="flex flex-col gap-3">
+                            {data?.map(room => {
+                                return <Link to={`/rooms/${room.id}`} className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent" key={room.id}>
+                                    <div className="flex-1 flex flex-col gap-1">
+                                        <h3 className="font-medium">{room.name}</h3>
+                                        
+                                        <div className="flex items-center gap-2">
+                                            <Badge variant="secondary">{room.questionsCount} pergunta(s)</Badge>
+                                        </div>
+                                    </div>
 
-            <div>
-                <Link className="underline" to="/room">Acessar sala</Link>
+                                    <span className="flex items-center gap-1 text-sm">
+                                        Entrar
+                                        <ArrowRight className="size-3"/>
+                                    </span>
+                                </Link>
+                            })}
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     )
